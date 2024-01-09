db0c394e0477bf8ab34f1dd95c8d5b67fa3f7a17
LouV31 <luigi.valentino99@yahoo.it>
2024-01-09 15:32:39 +0100

    Consegna

---
+++ bookshop-3.0-hooks/src/App.jsx
@@ -0,0 +1,27 @@
+//import logo from "./logo.svg";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import MyNav from "./components/MyNav";
+import Welcome from "./components/Welcome";
+import MyFooter from "./components/MyFooter";
+import BookList from "./components/BookList";
+import fantasy from "./data/books/fantasy.json";
+import history from "./data/books/history.json";
+import horror from "./data/books/horror.json";
+import romance from "./data/books/romance.json";
+import scifi from "./data/books/scifi.json";
+
+const allTheBooksArr = [...fantasy, ...history, ...horror, ...romance, ...scifi];
+
+function App() {
+    return (
+        <div className="App">
+            <MyNav brand="EpiBooks!" />
+            <Welcome title="libreria epica!" subtitle="Ma quanti bei libri!" />
+            <BookList booksArray={allTheBooksArr} />
+            <MyFooter className="container-md" />
+        </div>
+    );
+}
+
+export default App;
